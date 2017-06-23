9de44bd0099a85ccf5dac20fca8b9379fb95049e
Purasri.p <Purasri.p@192.168.2.87>
2017-06-23 11:26:05 +0700

    SignUpService.java - edit code UserDao.java - edit code

--- src/com/scap/testweb/service/SignUpService.java
+++ src/com/scap/testweb/service/SignUpService.java
@@ -1,43 +1,44 @@
 package com.scap.testweb.service;
 
+import com.scap.testweb.dao.UserDao;
+
 public class SignUpService {
-	
 	public static void main(String[] args) {
 		SignUpService service = new SignUpService();
-		//DbConnector dbconn = new DbConnector();
-		//dbconn.doConnect();
-		//boolean authen = service.userSignUp("aaaaaa", "1234");
+		
+		
 		boolean chkEmail = service.checkEmail("1234@gmail.com");
 		boolean chkPwd = service.chackPassword("1234Test#");
-		//System.out.println(authen);
+	
 		System.out.println(chkEmail);
 		System.out.println(chkPwd);
 	}
-/*
-	public boolean userSignUp(String pEmail, String password) {
-		boolean status = false;
-		if (pEmail != null && !pEmail.isEmpty() ) {
-			// dao
-		}
-		return status;
-	}
-*/
+
 	public boolean checkEmail(String email){
-		if(email == "1234@gmail.com"){
-			
-			return true;
+		if(email != null){
+			UserDao userDao = new UserDao();
+			String chkEmail = userDao.getEmail(email);
+			if(email == chkEmail){
+				return true;
+			}
+			else
+				return false;
 		}
 		else
 			return false;
 	}
+	
 	public boolean chackPassword(String password){
 		String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}";
 		if(password.matches(pattern) == true){
-			
 			return true;
 		}
-			
 		else
 			return false;
 	}
+	
+	public boolean setSignUp(){
+		return true;
+	}
+	
 }
