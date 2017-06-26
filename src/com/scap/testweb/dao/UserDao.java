df762323c32c9fea345367d76a6f61c090e570fb
Purasri.p <Purasri.p@192.168.2.87>
2017-06-26 09:38:30 +0700

    userDao - edit code Sign up - edit code

--- src/com/scap/testweb/dao/UserDao.java
+++ src/com/scap/testweb/dao/UserDao.java
@@ -1,27 +1,43 @@
 package com.scap.testweb.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.scap.testweb.utils.DbConnector;
 
 public class UserDao {
-	public String getEmail(String mail){
-		String sql = "select email form userAccount where email = mail";
- 
+	
+	public ArrayList<HashMap<String,String>> getEmail(String mail){
+		String sql = "SELECT EMAIL FROM LEAVE_MST_SIGNUP_PEAR WHERE EMAIL='" + mail + "'";
+		
 		DbConnector dbconn = new DbConnector();
+		ArrayList<HashMap<String,String>> resultDataEmail = null;
 		try{
 			dbconn.doConnect();
+			resultDataEmail = dbconn.getData(sql);
 		}
-		catch (Exception e) { System.out.println(e); }
-		//String sql = "1234@gmail.com";
- 
-		return sql;
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+			}
+		return resultDataEmail;
 	}
 	
-	public void setUserAccount(String userPassword, String userEmail){
-		String accout = "INSERT TABLE USERACCOUNT(USER_EMAIL, USER_PASSWORD, DATE)VALUES('userEmail','userPassword');";
+	public ArrayList<HashMap<String,String>> setUserAccount(String userEmail, String userPassword){
+		String account = "INSERT INTO LEAVE_MST_SIGNUP_PEAR(EMAIL, PASSWORD) VALUES('"+userEmail+"','"+userPassword+"')";
+		String chkAccount = "SELECT * FROM LEAVE_MST_SIGNUP_PEAR WHERE EMAIL='" + userEmail + "'";
 		DbConnector dbconn = new DbConnector();
+		ArrayList<HashMap<String,String>> resultDataAccount = null;
 		try{
 			dbconn.doConnect();
+			dbconn.doSave(account);
+			dbconn.doCommit();
+			resultDataAccount = dbconn.getData(chkAccount);
 		}
-		catch (Exception e) { System.out.println(e); }
+		catch (Exception e) { 
+			e.printStackTrace();
+			System.out.println(e);
+			}
+		return resultDataAccount;
 	}
 }
--- src/com/scap/testweb/service/SignUpService.java
+++ src/com/scap/testweb/service/SignUpService.java
@@ -1,24 +1,45 @@
 package com.scap.testweb.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.scap.testweb.dao.UserDao;
 
 public class SignUpService {
 	public static void main(String[] args) {
 		SignUpService service = new SignUpService();
-		
-		
-		boolean chkEmail = service.checkEmail("1234@gmail.com");
-		boolean chkPwd = service.chackPassword("1234Test#");
-	
-		System.out.println(chkEmail);
-		System.out.println(chkPwd);
+		String email = "pear11@gmail.com";
+		String passwd = "1234Test#";
+		boolean chkEmail = service.checkEmail(email);
+		boolean chkPwd = service.chackPassword(passwd);  
+		if(chkEmail == false && chkPwd == true){
+			System.out.println("E-mail : Unique email");
+			System.out.println("Password : Approve password");
+			boolean createUser = service.createAccount(email,passwd);
+			if(createUser == true){
+				System.out.println("Sign up success!!");
+			}
+			else
+				System.out.println("Sign up failed!!");
+		}
+		else{
+			if(chkEmail == true && chkPwd == true){
+				System.out.println("E-mail : Duplicate email");
+				System.out.println("Password : Approve password");
+			}
+			if(chkEmail == false && chkPwd == false){
+				System.out.println("E-mail : Unique email");
+				System.out.println("Password : Do not approve");
+			}
+		}
 	}
 
 	public boolean checkEmail(String email){
 		if(email != null){
 			UserDao userDao = new UserDao();
-			String chkEmail = userDao.getEmail(email);
-			if(email == chkEmail){
+			ArrayList<HashMap<String,String>> chkEmail = userDao.getEmail(email);
+			int sizeOfData = chkEmail.size();
+			if(sizeOfData == 1){
 				return true;
 			}
 			else
@@ -37,8 +58,15 @@ public class SignUpService {
 			return false;
 	}
 	
-	public boolean setSignUp(){
-		return true;
+	public boolean createAccount(String email,String passwd){
+		UserDao userDao = new UserDao();
+		ArrayList<HashMap<String,String>> chkCorrect = userDao.setUserAccount(email,passwd);
+		//System.out.println(chkCorrect);
+		int sizeOfDataUser = chkCorrect.size();
+		if(sizeOfDataUser == 1){
+			return true;
+		}
+		else
+			return false;
 	}
-	
 }
