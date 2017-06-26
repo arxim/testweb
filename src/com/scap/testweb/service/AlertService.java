26a81847cd3943a76f681ec7097746ba36c6a314
WichitaW <WichitaW@192.168.2.124>
2017-06-26 13:42:26 +0700

    Update AlertService.java

--- src/com/scap/testweb/service/AlertService.java
+++ src/com/scap/testweb/service/AlertService.java
@@ -1,7 +1,7 @@
 package com.scap.testweb.service;
 
-import java.text.DateFormat;
-import java.text.ParseException;
+
+import java.text.DateFormat; 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,98 +14,93 @@ public class AlertService {
 	public static void main(String[] args) {
 		AlertService service = new AlertService();
 		
-	//	UPDATE Date in database
-	//	SELECT curdate(); <= Date
+		String email = "yui@gmail.com";
 		
-		String upDate = "20170615";
-		System.out.println("Update : " + upDate);
-		DateFormat df = new SimpleDateFormat("yyyyMMdd");
+/*	//	UPDATE Date in database
+		String sql = "INSERT INTO ALERT (UPDATE) VALUES getdate() WHERE EMAIL='" + email + "'"; ;
+		DbConnector dbconn = new DbConnector();
+		dbconn.doConnect();
 		
-		try {
-				Date date = df.parse(upDate);
-				Calendar c = Calendar.getInstance();
-				c.setTime(date);
-				
-			//	System.out.println("Full Date : " + date);
-			//	System.out.println("Time : " + date.getTime());
-				
-				int year = c.get(Calendar.YEAR);
-			//	System.out.println("Year : " + year);
-				
-				int month = c.get(Calendar.MONTH) + 1;
-			//	System.out.println("Month : " + month);
-				
-				int day = c.get(Calendar.DATE);
-			//	System.out.println("Day : " + day);
-				
-				
-			//	System.out.println("Hour : " + c.get(Calendar.HOUR_OF_DAY));
-			//	System.out.println("Minute : " + c.get(Calendar.MINUTE));
-			//	System.out.println("Second : " + c.get(Calendar.SECOND));
-				
-				boolean calculate = service.calculate(year, month, day);
-				
-				if(calculate == true){
-					System.out.println("Welcome.");
-				}
-				else{
-					System.out.println("You have been using your password for more than 90 days. Please change your password.");
-				}
-				
-			} catch(ParseException e) {
-				e.printStackTrace();
-				System.out.println(e);
-			}
-	}
+	//	database	
+		dbconn.doDisconnect();	*/
+		
 	
-	public boolean calculate(int year, int month, int day){
-	//	SELECT Date from database	
-	//	String pDate = getDate("yui@gmail.com");
-		
-		String pDate = "20170315";
-		System.out.println("  Date : " + pDate);
-		DateFormat df = new SimpleDateFormat("yyyyMMdd");
+	//	String upDate = "20170520";
+	//	System.out.println("Update : " + upDate);
 		
 		try{
-				Date date = df.parse(pDate);
-				Calendar c = Calendar.getInstance();
-				c.setTime(date);
+			
+			Date dNow = new Date();
+			DateFormat df = new SimpleDateFormat("yyyyMMdd");
+			System.out.println("Update : " + df.format(dNow));
+
+			Calendar c = Calendar.getInstance();
+					
 		
-				int pastyear = c.get(Calendar.YEAR);
-			//	System.out.println("Year : " + pastyear);
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH) + 1;
+			int day = c.get(Calendar.DATE);
 		
-				int pastmonth = c.get(Calendar.MONTH) + 1;
-			//	System.out.println("Month : " + pastmonth);
-		
-				int pastday = c.get(Calendar.DATE);
-			//	System.out.println("Day : " + pastday);
+			boolean calculate = service.calculate(email, year, month, day);
 				
-				if(year == pastyear ){
-					if(month == pastmonth){
+			if(calculate == true){
+				System.out.println("Welcome.");
+			}
+			else{
+				System.out.println("You have been using your password for more than 90 days. Please change your password.");
+			}
+		}	catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+		   
+		}
+	
+	}
+	
+	public boolean calculate(String email, int year, int month, int day){
+	//	SELECT Date from database	
+		ArrayList<HashMap<String, String>> dPast = getDate(email);
+		
+	//	String dPast = "20170326";
+		
+		
+		try{
+			System.out.println("  Date : " + dPast);
+			DateFormat df = new SimpleDateFormat("yyyyMMdd");
+			System.out.println("Update : " + df.format(dPast));
+			
+			Calendar c = Calendar.getInstance();
+			
+		
+			int pastyear = c.get(Calendar.YEAR);		
+			int pastmonth = c.get(Calendar.MONTH) + 1;
+			int pastday = c.get(Calendar.DATE);
+				
+			if(year == pastyear ){
+				if(month == pastmonth){
+					return true;
+				}
+				else if(month != pastmonth){
+					if((month - pastmonth) < 3){
 						return true;
 					}
-					else if(month != pastmonth){
-						if((month - pastmonth) < 3){
+					else if((month - pastmonth) > 3){
+						return false;
+					}
+					else if((month - pastmonth) == 3){
+						if(day < pastday){
 							return true;
 						}
-						else if((month - pastmonth) > 3){
+						else if(day >= pastday){
 							return false;
 						}
-						else if((month - pastmonth) == 3){
-							if(day < pastday){
-								return true;
-							}
-							else if(day >= pastday){
-								return false;
-							}
-						}
 					}
-					
 				}
-				else
-					return true;
-		
-		}catch(ParseException e) {
+				
+			}
+			else
+				return false;		
+		}catch(Exception e) {
 			e.printStackTrace();
 			System.out.println(e);
 		}
@@ -113,21 +108,22 @@ public class AlertService {
 	
 	}
 
-/*	private String getDate(String mail) {
-		String sql = "SELECT DATE FROM ALERT WHERE EMAIL='" + email + "'"; ;
+	private ArrayList<HashMap<String, String>> getDate(String email) {
+		String sql = "SELECT CREATE_DATE FROM LEAVE_MST_SIGNUP_PEAR WHERE EMAIL='" + email + "'"; ;
 		DbConnector dbconn = new DbConnector();
-		String pDate = null;
+		ArrayList<HashMap<String, String>> dPast = null;
 		try{
 			dbconn.doConnect();
 			
-			pDate = dbconn.getData(sql);
+			dPast = dbconn.getData(sql);
+			return dPast;
 		
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e);
 			}
-		return pDate;
-	}		*/
+		return dPast;
+	}		
 		
 }
